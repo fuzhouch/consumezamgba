0213465e46912df98ad69c93eef46150485c1fed
fuzhou.chen <fuzhouch@gmail.com>
2024-02-06 00:42:00 +0800

    [NEW] Use readable build file parameters.

--- build.zig
+++ build.zig
@@ -3,11 +3,20 @@ const gba = @import("zamgba");
 
 // Use gba.arm.addROM() to define your target. This function defines
 // necessary ARM target and steps to create ROM image (*.gba)
-// when installing artifact. Reference to ``zamgba/src/build/arm.zig``
-// for details.
-
+// when installing artifact. Reference to zamgba project,
+// ``src/build/arm.zig`` for details.
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
-    const exe = gba.arm.addROM(b, optimize, "testcli", "src/main.zig"); 
-    b.installArtifact(exe);
+
+    const zamgba = b.dependency("zamgba", .{});
+    var it = zamgba.builder.modules.iterator();
+    while (it.next()) |entry| {
+        std.debug.print("{s}", .{entry.key_ptr});
+    }
+    const rom = gba.arm.addROM(b, .{
+        .optimize = optimize,
+        .name = "first",
+        .root_source_file = "src/main.zig",
+    });
+    b.installArtifact(rom);
 }
